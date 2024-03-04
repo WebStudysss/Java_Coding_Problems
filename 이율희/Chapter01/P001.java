d0ef667435aa9bde02d61d8a9d96cccebc64eaf6
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2024-03-04 09:47:09 +0900

    Update P001.java
    클래스명 변경

--- 이율희/Chapter01/P001.java
+++ 이율희/Chapter01/P001.java
@@ -1,14 +1,14 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class C01_001 {
+public class P001 {
 
     // 문자열의 공백 제거, 대/소문자 통일해서 count
 
     String str;
     Map<Character, Integer> countMap;
 
-    public C01_001 ( String str ) {
+    public P001 ( String str ) {
         this.str = str.replaceAll(" ", "").toLowerCase();
         this.countMap = new HashMap<>();
     }
