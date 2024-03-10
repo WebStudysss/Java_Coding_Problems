2614af7af3d9d65370846b3e97d3e6463af12f7a
tkdtn4657 <75903442+tkdtn4657@users.noreply.github.com>
2024-03-10 18:22:46 +0900

    Create P004.java

---
+++ 이상수/Chapter01/P004.java
@@ -0,0 +1,16 @@
+import java.util.OptionalInt;
+
+public class P004 {
+
+    public boolean solution(String s){
+
+        OptionalInt any = s.chars().filter(this::isNotDigit).findAny();
+        
+        //숫자만 있으면 true
+        return any.isEmpty();
+    }
+
+    private boolean isNotDigit(int c){
+        return !Character.isDigit(c);
+    }
+}
