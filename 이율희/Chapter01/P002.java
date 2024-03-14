69eadac5b16a980e2aca0f0bdee22a1ee11c561c
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2024-03-14 13:11:42 +0900

    Update P002.java

--- 이율희/Chapter01/P002.java
+++ 이율희/Chapter01/P002.java
@@ -1,17 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class P002 {
     String str;
+    Map<Character, Integer> countStrMap;
 
     public P002 (String str) {
         this.str = str;
+        countStrMap = new HashMap<>();
+        countLetter();
+    }
+
+    // 반복이 한 번도 안 되는 경우 : abcd
+    // 중간 글자가 반복되는 경우 : abcccd
+    // 반복되는 글자가 많은 경우 : aaabbbcccd
+    // 반복 안 되는 글자가 하나도 없는 경우 : aaabbbcccddd
+
+    private void countLetter() {
+
+        // 개수 count
+        for (char c : str.toCharArray()) {
+            countStrMap.put(c, countStrMap.getOrDefault(c, 0) + 1 );
+        }
     }
 
     private String checkNonRepeatLetter() {
-        
-        char c = str.charAt(0);
 
-        for (int i = 1; i < str.length(); i++) {
-            if(c != str.charAt(i)) {
-                return String.valueOf(str.charAt(i));
+        Character repeatLetter = null;
+        char nowLetter;
+
+        for(int i = 0; i < str.length(); i++) {
+            nowLetter = str.charAt(i);
+
+            if(repeatLetter != null && countStrMap.get(nowLetter) == 1) {
+                return String.valueOf(nowLetter);
+            }
+
+            if (countStrMap.get(nowLetter) > 1) {
+                repeatLetter = nowLetter;
             }
         }
 
