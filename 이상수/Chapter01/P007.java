62bbe8f8e43f585654abd3a22e16fc1bad0edf64
tkdtn4657 <75903442+tkdtn4657@users.noreply.github.com>
2024-04-07 16:36:04 +0900

    Create P007.java

---
+++ 이상수/Chapter01/P007.java
@@ -0,0 +1,12 @@
+public class P007 {
+
+    public String solution(String s) {
+
+        Integer intString = Integer.parseInt(s);
+        Long longString = Long.parseLong(s);
+        Float floatString = Float.parseFloat(s);
+        Double doubleString = Double.parseDouble(s);
+
+        return s;
+    }
+}
